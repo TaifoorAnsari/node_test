955b2b2fdb02bc31f8a8c7fa17c983915cbf31cf
shruthi1798 <shruthi1798@gmail.com>
2026-06-05 10:20:33 +0530

    File changed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/todos', (req, res) => res.json(todos));
 app.post('/todos', (req, res) => {
   todos.push(req.body.task);
-  res.json({ message: 'Task added' });
+  res.json({ message: 'Task add' });
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
