6fcb211da505ecf0d0e1b06c51fe3115cc726b1e
shruthi1798 <shruthi1798@gmail.com>
2026-06-05 10:33:04 +0530

    SCRUM-2 Task 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/todos', (req, res) => res.json(todos));
 app.post('/todos', (req, res) => {
   todos.push(req.body.task);
-  res.json({ message: 'Task add' });
+  res.json({ message: 'Tasks 1 add' });
 });
 
 app.listen(3000, () => console.log('web page running on port 3000'));
